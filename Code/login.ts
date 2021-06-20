3d5aaadf5088c8ccadfa66eea1af913990aad679
annaalehmann <64208557+annaalehmann@users.noreply.github.com>
2021-06-20 20:04:28 +0200

    23456

--- Code/login.ts
+++ Code/login.ts
@@ -1,12 +1,12 @@
 namespace pruefungsabgabe {
 
     /*Variablen Deklaration für den Login-BUtton*/
-    let buttonLogin: HTMLButtonElement = (<HTMLButtonElement>document.getElementById("login"));
+    let buttonLogin: HTMLButtonElement = <HTMLButtonElement>document.getElementById("login");
     /*Beim klicken auf den Button wird die Funktione handleLogin durchgeführt*/
     buttonLogin.addEventListener("click", handleLogin);
 
     /*Variablen Deklaration für den Registrieren-BUtton*/
-    let buttonRegistrierung: HTMLButtonElement = (<HTMLButtonElement>document.getElementById("registrierung"));
+    let buttonRegistrierung: HTMLButtonElement = <HTMLButtonElement>document.getElementById("registrierung");
     /**Beim klicken auf den Button wird die Funktione handleLogin durchgeführt**/
     buttonRegistrierung.addEventListener("click", handleRegistrierung);
 
