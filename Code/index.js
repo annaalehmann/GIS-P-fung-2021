478809adef7f6f8f9dfc986d1193c3089b133baa
annaalehmann <64208557+annaalehmann@users.noreply.github.com>
2021-06-26 17:47:24 +0200

    neue datei

---
+++ Code/index.js
@@ -0,0 +1,34 @@
+"use strict";
+var pruefungsabgabe;
+(function (pruefungsabgabe) {
+    let rezeptArray;
+    handlePublishAllRecipes();
+    async function handlePublishAllRecipes() {
+        let formData = new FormData(document.forms[0]);
+        let query = new URLSearchParams(formData);
+        /* let url: string = "https://gis-pruefung-2021.herokuapp.com";*/
+        let _url = "http://localhost:8100";
+        _url += "/publishAllRecipe" + "?" + query.toString();
+        let response = await fetch(_url);
+        let responseJSON = await response.json();
+        rezeptArray = JSON.parse(JSON.stringify(responseJSON));
+        document.getElementById("allRecipes").innerHTML = "";
+        for (let i = 0; i < rezeptArray.length; i++) {
+            let div = document.createElement("div");
+            div.id = "currentRecipeDiv" + i;
+            div.classList.add("currentRecipeClass");
+            div.setAttribute("zaehler", i.toString());
+            document.getElementById("myRecipes").appendChild(div);
+            let zutaten = document.createElement("p");
+            zutaten.innerHTML = "Zutaten: " + rezeptArray[i].zutaten;
+            div.appendChild(zutaten);
+            let zubereitung = document.createElement("p");
+            zubereitung.innerHTML = "Zubereitung: " + rezeptArray[i].zubereitung;
+            div.appendChild(zubereitung);
+            let autor = document.createElement("p");
+            autor.innerHTML = "Autor: " + rezeptArray[i].autor;
+            div.appendChild(autor);
+        }
+    }
+})(pruefungsabgabe || (pruefungsabgabe = {}));
+//# sourceMappingURL=index.js.map
---
+++ Code/index.ts
@@ -0,0 +1,43 @@
+namespace pruefungsabgabe {
+
+    let rezeptArray: Rezepte[];
+
+    handlePublishAllRecipes();
+
+    async function handlePublishAllRecipes(): Promise<void> {
+
+        let formData: FormData = new FormData(document.forms[0]);
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+
+        /* let url: string = "https://gis-pruefung-2021.herokuapp.com";*/
+        let _url: string = "http://localhost:8100";
+        _url += "/publishAllRecipe" + "?" + query.toString();
+
+        let response: Response = await fetch(_url);
+        let responseJSON: string = await response.json();
+        rezeptArray = JSON.parse(JSON.stringify(responseJSON));
+
+        document.getElementById("allRecipes").innerHTML = "";
+
+        for (let i: number = 0; i < rezeptArray.length; i++) {
+
+            let div: HTMLDivElement = document.createElement("div");
+            div.id = "currentRecipeDiv" + i;
+            div.classList.add("currentRecipeClass");
+            div.setAttribute("zaehler", i.toString());
+            document.getElementById("myRecipes").appendChild(div);
+
+            let zutaten: HTMLElement = document.createElement("p");
+            zutaten.innerHTML = "Zutaten: " + rezeptArray[i].zutaten;
+            div.appendChild(zutaten);
+
+            let zubereitung: HTMLElement = document.createElement("p");
+            zubereitung.innerHTML = "Zubereitung: " + rezeptArray[i].zubereitung;
+            div.appendChild(zubereitung);
+
+            let autor: HTMLElement = document.createElement("p");
+            autor.innerHTML = "Autor: " + rezeptArray[i].autor;
+            div.appendChild(autor);
+        }
+
+    }
